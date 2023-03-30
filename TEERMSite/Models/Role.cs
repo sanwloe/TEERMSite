9e0ba30680e14640b0cd1449a3dc48491c547942
Oleksandr <oleksandr.ill.x@gmail.com>
2023-03-30 18:16:29 +0300

    Test

--- TEERMSite/Models/Role.cs
+++ TEERMSite/Models/Role.cs
@@ -7,6 +7,7 @@ namespace TEERMSite.Models
         [Required]
         public int Id { get; set; }
         [Required]
-        public string Name { get; set; }    
+        public string Name { get; set; }
+        public string Description { get; set; }
     }
 }
