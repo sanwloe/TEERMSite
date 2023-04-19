0178d82974c0d5d646bfafd68d11830a6ece2509
Oleksandr <oleksandr.ill.x@gmail.com>
2023-04-19 18:04:12 +0300

    Поправки

--- TEERMSite/ClientApp/src/environments/environment.prod.ts
+++ TEERMSite/ClientApp/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  BASEURI : "https://termm.lntu.edu.ua"
+  BASEURI: "https://termm.lntu.edu.ua"
+  // BASEURI : "https://localhost:1555"
 };
--- TEERMSite/Program.cs
+++ TEERMSite/Program.cs
@@ -27,6 +27,7 @@ namespace TEERMSite
                 options.AddPolicy("AllowSpecificOrigin",
                     builder => builder.WithOrigins("https://localhost:44403", "https://localhost:1555", "https://localhost:4200", "https://termm.lntu.edu.ua").AllowAnyMethod().AllowAnyHeader());
             });
+            
 
             var app = builder.Build();
             
@@ -38,11 +39,21 @@ namespace TEERMSite
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
                 app.UseCors("AllowSpecificOrigin");
+
+                app.UseHttpsRedirection();
+                app.UseStaticFiles();
+
+                app.UseCors("AllowSpecificOrigin");
+                app.UseRouting();
+
+
+                app.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{action=Index}/{id?}");
             }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseCors("AllowSpecificOrigin");
             app.UseRouting();
 
